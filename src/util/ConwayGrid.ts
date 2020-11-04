1e072bd36d84c2e0bfef771c9add2938ec743661
kodejuice <kodejuice@gmail.com>
2020-11-04 23:14:06 +0100

    doc

--- src/util/ConwayGrid.ts
+++ src/util/ConwayGrid.ts
@@ -47,23 +47,50 @@ class ConwayGrid extends Matrix<boolean> {
         this.alive_cells = new Set();
     }
 
+    /**
+     * return alive cells set
+     */
     alive() {
         return this.alive_cells;
     }
 
+    /**
+     * return dead cells set
+     */
     dead() {
         return this.dead_cells;
     }
 
+    /**
+     * return the index of a cell in a grid as string "i:j"
+     * @param {number} i row index
+     * @param {number} j column index
+     */
     cell_index(i:number, j: number) {
         return `${i}:${j}`;
     }
 
+    /**
+     * set the value of a grid, then check
+     *  if the cell is alive, update stat accordinly
+     * 
+     * @param {number}  i     row index
+     * @param {number}  j     col index
+     * @param {boolean} value cell value
+     * @override super.set
+     */
     set(i: number, j: number, value: boolean) {
         super.set(i, j, value);
         this.$monitor_cell_life(i, j);
     }
 
+    /**
+     * toggle cell value,
+     *   true=>false, false=>true
+     *
+     * @param {number} i row index
+     * @param {number} j column index
+     */
     toggle(i:number, j:number) {
         var val = this.get(i, j);
         if (val) {
@@ -73,6 +100,11 @@ class ConwayGrid extends Matrix<boolean> {
         }
     }
 
+    /**
+     * scale up/down matrix
+     *
+     * @param {'up'|'down'} direction
+     */
     scale(direction: 'up'|'down') {
         if (direction == 'up' && this.can_scale('up')) {
             this.$scale(this.$scale_factor + 1 as SCALE);
@@ -83,27 +115,49 @@ class ConwayGrid extends Matrix<boolean> {
         return this;
     }
 
+    /**
+     * Determines ability to scale.
+     *
+     * @param      {'up'|'down'}   direction  The direction
+     * @return     {boolean}  True if able to scale, False otherwise.
+     */
     can_scale(direction: 'up'|'down'): boolean {
         if (direction == 'up') return this.$scale() < SCALES.length-1;
         else return this.$scale() > 1;
     }
 
+    /**
+     * return currently applied scale factor
+     * @return {SCALE}
+     */
     scale_factor(): SCALE {
         return this.$scale_factor;
     }
 
+    /**
+     * update population sets, so vue can update dom accordinly
+     */
     update_census() {
         // this lets Vue know about changes in the sets
         this.alive_cells = new Set(this.alive_cells);
         this.dead_cells = new Set(this.dead_cells);
     }
 
+    /**
+     * clears the grid
+     */
     clear_grid() {
         this.clear();
         this.$update_grid_stat(false);
     }
 
-    private $scale(factor?: 1|2|3|4|5|6|7) {
+    /**
+     * scale grid, sets the height/width of grid
+     * according to selected scale factor
+     *
+     * @param {SCALE} factor
+     */
+    private $scale(factor?: SCALE) {
         if (!factor) return this.$scale_factor;
 
         const {rows, cols} = SCALES[factor];
@@ -115,6 +169,13 @@ class ConwayGrid extends Matrix<boolean> {
         return this.$scale_factor = factor;
     }
 
+    /**
+     * resets population sets,
+     * then go over each cell, check if the 
+     * cell is dead/alive cells and update appropriate sets
+     *
+     * @param      {boolean}  check_cells  should we go over each cell?
+     */
     private $update_grid_stat(check_cells = true) {
         this.alive_cells = new Set();
         this.dead_cells = new Set();
@@ -127,6 +188,13 @@ class ConwayGrid extends Matrix<boolean> {
         }
     }
 
+    /**
+     * check if a cell is dead or alive, then update population sets
+     *
+     * @param {number}  i                   row index
+     * @param {number}  j                   column index
+     * @param {boolean} monitor_deaths      should we update if its a dead cell? (dead_cells)
+     */
     private $monitor_cell_life(i:number, j:number, monitor_deaths=true) {
         const index = this.cell_index(i, j);
         if (this.get(i, j) == true) { // alive
